9c1117689e7196fc778e0baec6950d2977b040b0
japhet67 <japhetagbalegno67@gmail.com>
2025-03-18 23:31:49 +0000

    Update config.cjs

--- config.cjs
+++ config.cjs
@@ -2,7 +2,7 @@ const fs = require('fs');
 if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env' });
 
 module.exports = {
-  "SESSION_ID": process.env.SESSION_ID || "",
+  "PRABATH-MD~CGoEnabA#emVfr_JhiNiRqpf2WkY_pkMWfqIYfaojeOmiDWIT5JE": process.env.SESSION_ID || "",
   "ALIVE_IMG": process.env.ALIVE_IMG || "https://raw.githubusercontent.com/HyHamza/HyHamza/refs/heads/main/Images/logo.jpg",
   "PREFIX": process.env.PREFIX || ".",
   "AUTO_READ_STATUS": process.env.AUTO_READ_STATUS === 'true',
